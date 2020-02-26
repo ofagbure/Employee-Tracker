3d12070c40d24e165a7c3dffcdaba0b6ec5672e0
Femi Fagbure <femifagbure@gmail.com>
2020-02-25 21:42:03 -0500

    cleaned up code

--- employees_db.sql
+++ employees_db.sql
@@ -17,10 +17,12 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30)
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
