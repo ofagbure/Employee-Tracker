5976afdd1f40a30651cccb322b66875b6286f4ca
Femi Fagbure <femifagbure@gmail.com>
2020-02-25 20:40:19 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# Employee-Tracker
+# Employee Tracker
 A simple app that records employee data. User is able to view, add, and update employee records.
+
+## Table of Contents 
+
+* [User Story](#UserStory)
+* [Functionality](#Functionality)
+* [License](#license)
+* [Badges](#Badges)
+
+## User Story
+As a business owner
+I want to be able to view and manage the departments, roles, and employees in my company
+So that I can organize and plan my business
+
+## Functionality
+When a user begins the application, they are prompted to choose between viewing, updating, deleting, or adding items. 
+Users can;
+* Update employee's role, department, and managers
+* Add new employees, roles, or departments
+* Delete an employee
+* View all records (employees, roles, and departments)
+
+## License
+
+MIT License
+
+Copyright (c) [2020] [Olufemi Fagbure]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+## Badges
+
+![GitHub last commit](https://img.shields.io/github/last-commit/ofagbure/Employee-Tracker)
---
+++ index.js
@@ -0,0 +1,184 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "employees_DB"
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    start();
+});
+
+
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "Would you like to [VIEW EMPLOYEE RECORDS], [UPDATE AN EMPLOYEE], [ADD ITEMS], or [DELETE AN EMPLOYEE]?",
+            choices: ["VIEW ITEMS", "UPDATE ITEMS", "ADD ITEMS", "DELETE AN EMPLOYEE"]
+        })
+        .then(function (answer) {
+
+            if (answer.action === "ADD ITEMS") {
+                addRecord();
+            }
+            else if (answer.action === "UPDATE AN EMPLOYEE") {
+                updateRecord();
+            }
+            else if (answer.action === "DELETE AN EMPLOYEE") {
+                deleteRecord();
+
+            }
+            else if (answer.action === "VIEW ITEM") {
+                viewRecord();
+            }
+
+            else {
+                connection.end();
+            }
+        });
+};
+
+
+function addRecord() {
+    inquirer
+        .prompt([
+            {
+                name: "item",
+                type: "list",
+                message: "Would you like to add an [EMPLOYEE], [DEPARTMENT], or [ROLE]?",
+                choices: ["EMPLOYEE", "DEPARTMENT", "ROLE"]
+            }])
+        .then(function (answer) {
+            // still need to define below functions
+            if (answer.item === "EMPLOYEE") {
+                addEmployee();
+            }
+            else if (answer.item === "DEPARTMENT") {
+                addDepartment();
+            }
+            else if (answer.item === "ROLE") {
+                addRole();
+            }
+            else {
+                connection.end();
+            }
+            start();
+        })
+};
+
+function updateRecord() {
+    inquirer
+        .prompt([
+            {
+                name: "employee",
+                type: "input",
+                message: "Who would you like to update? (please provide their employee ID)"
+            },
+            {
+                name: "roleID",
+                type: "input",
+                message: "What is their new role? (please provide the role ID)"
+            },
+            {
+                name: "managerID",
+                type: "input",
+                message: "Who is their new manager? (please provide the employee ID of the manager",
+            }
+        ])
+        .then(function (answer) {
+            connection.query(
+                "UPDATE employee SET ? WHERE ?",
+                [
+                    {
+                        employee_id: answer.employee
+                    },
+                    {
+                        role_id: answer.roleID
+                    },
+                    {
+                        manager_id: answer.managerID
+                    }
+                ],
+                function (err) {
+                    if (err) throw err;
+                    console.log("The employee was updated successfully!");
+                }
+            );
+        });
+    start();
+};
+
+function deleteRecord() {
+    inquirer
+        .prompt([
+            {
+                name: "item",
+                type: "list",
+                message: "Would you like to delete an [EMPLOYEE], [DEPARTMENT], or [ROLE]?",
+                choices: ["EMPLOYEE", "DEPARTMENT", "ROLE"]
+            }])
+        .then(function (answer) {
+            // still need to define below functions
+            if (answer.item === "EMPLOYEE") {
+                deleteEmployee();
+            }
+            else if (answer.item === "DEPARTMENT") {
+                deleteDepartment();
+            }
+            else if (answer.item === "ROLE") {
+                deleteRole();
+            }
+            else {
+                connection.end();
+            }
+        })
+    start();
+};
+
+function viewRecord() {
+    inquirer
+        .prompt([
+            {
+                name: "item",
+                type: "list",
+                message: "Would you like to view all [EMPLOYEES], [DEPARTMENTS], or [ROLES]?",
+                choices: ["EMPLOYEES", "DEPARTMENTS", "ROLES"]
+            }])
+        .then(function (answer) {
+            if (answer.item === "EMPLOYEES") {
+                connection.query("SELECT * FROM employee", function (err, res) {
+                    if (err) throw err;
+                    console.log(res);
+                    connection.end();
+                });
+            }
+            else if (answer.item === "DEPARTMENTS") {
+                connection.query("SELECT * FROM department", function (err, res) {
+                    if (err) throw err;
+                    console.log(res);
+                    connection.end();
+                });
+            }
+            else if (answer.item === "ROLES") {
+                connection.query("SELECT * FROM role", function (err, res) {
+                    if (err) throw err;
+                    console.log(res);
+                    connection.end();
+                });
+            }
+            else {
+                connection.end();
+            }
+        })
+    start();
+};
+
