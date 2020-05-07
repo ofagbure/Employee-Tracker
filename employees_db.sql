592783430d263c950b1c38fc3442d50264cb030f
Femi Fagbure <femifagbure@gmail.com>
2020-05-06 20:31:35 -0400

    added seeds file

--- employees_db.sql
+++ employees_db.sql
@@ -10,7 +10,7 @@ CREATE TABLE employee (
   last_name VARCHAR(30) NOT NULL,
   role_id INT NOT NULL,
   manager_id INT NOT NULL, 
-  PRIMARY KEY (id)
+  PRIMARY KEY (employee_id)
 );
 
 CREATE TABLE role (
---
+++ seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ("Femi", "Fagbure", 123, 452), 
+    ("Adam", "Ferguson", 245, 452),
+    ("Feyi", "Fagbure", 57392, 749);
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("Analyst", "300.55", 123), 
+    ("Associate", "573892.88", 123),
+    ("Vice President", "374802.00", 123);
+
+INSERT INTO department (name)
+VALUE ("Human Resources"), 
+    ("Internal Audit"),
+    ("Finance");
+
+
